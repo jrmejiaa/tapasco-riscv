a6da58e44633a581eaf38c4e17ca215e07d6637d
jrmejiaa <jairo.mejia.2708@gmail.com>
2023-04-18 23:07:28 +0200

    cache_availability: Add dict to know which core supports the CacheSystem

---
+++ common/cache_availability.tcl
@@ -0,0 +1,15 @@
+set is_cache_available [list \
+  "vexriscv_pe"     {false} \
+  "orca_pe"         {false} \
+  "orca_mxp_pe"     {false} \
+  "piccolo32_pe"    {true} \
+  "piccolo64_pe"    {true} \
+  "flute32_pe"      {false} \
+  "flute64_pe"      {false} \
+  "picorv32_pe"     {false} \
+  "swerv_pe"        {false} \
+  "scr1_pe"         {false} \
+  "cva6_pe"         {false} \
+  "swerv_eh2_pe"    {false} \
+  "cva5_pe"         {false} \
+]
