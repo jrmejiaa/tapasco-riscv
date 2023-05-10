29391dd62c27b1f3a98f58783172a710a51c4296
jrmejiaa <jairo.mejia.2708@gmail.com>
2023-05-10 10:06:49 +0200

    connect_common_ports: Connect extra AXI to convert lite to full

--- common/connect_common_ports.tcl
+++ common/connect_common_ports.tcl
@@ -6,11 +6,15 @@ connect_bd_net -net rst_CLK_100M_interconnect_aresetn [get_bd_pins axi_interconn
 connect_bd_net -net rst_CLK_100M_peripheral_aresetn [get_bd_pins AXIGate_0/RST_N] [get_bd_pins RVController_0/RST_N] [get_bd_pins axi_interconnect_0/M00_ARESETN] [get_bd_pins axi_interconnect_0/S00_ARESETN] [get_bd_pins axi_interconnect_0/S01_ARESETN] [get_bd_pins axi_interconnect_1/M00_ARESETN] [get_bd_pins axi_interconnect_1/M01_ARESETN] [get_bd_pins axi_interconnect_1/S00_ARESETN] [get_bd_pins axi_mem_intercon_1/M00_ARESETN] [get_bd_pins axi_mem_intercon_1/M0*_ARESETN] [get_bd_pins axi_mem_intercon_1/S0*_ARESETN] [get_bd_pins dmaOffset/RST_N] [get_bd_pins ps_dmem_ctrl/s_axi_aresetn] [get_bd_pins ps_imem_ctrl/s_axi_aresetn] [get_bd_pins rst_CLK_100M/peripheral_aresetn] [get_bd_pins rv_dmem_ctrl/RST_N] [get_bd_pins rv_imem_ctrl/RST_N]
 
 if {$maxi_ports == 2} {
-	connect_bd_net -net CLK_1 [get_bd_pins dmaOffset2/CLK]
-	connect_bd_net -net rst_CLK_100M_peripheral_aresetn [get_bd_pins dmaOffset2/RST_N]
+  connect_bd_net -net CLK_1 [get_bd_pins dmaOffset2/CLK]
+  connect_bd_net -net rst_CLK_100M_peripheral_aresetn [get_bd_pins dmaOffset2/RST_N]
 }
 
-if { $cache && [dict get $is_cache_available $project_name] } {
-	connect_bd_net -net CLK_1 [get_bd_pins cache_system_0/CLK]
-	connect_bd_net -net RVController_0_rv_rstn [get_bd_pins cache_system_0/RST_N]
+if { $set_cache_sys && [dict get $is_cache_available $project_name] } {
+  connect_bd_net -net CLK_1 [get_bd_pins cache_system_0/CLK]
+  connect_bd_net -net RVController_0_rv_rstn [get_bd_pins cache_system_0/RST_N]
+  if {![dict get $is_harvard_arch $project_name]} {
+    connect_bd_net [get_bd_ports CLK] [get_bd_pins axi_lite2full/ACLK] [get_bd_pins axi_lite2full/S00_ACLK] [get_bd_pins axi_lite2full/M00_ACLK]
+    connect_bd_net [get_bd_pins rst_CLK_100M/peripheral_aresetn] [get_bd_pins axi_lite2full/ARESETN] [get_bd_pins axi_lite2full/S00_ARESETN] [get_bd_pins axi_lite2full/M00_ARESETN]
+  }
 }
