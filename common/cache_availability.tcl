16e0c24b80dc6499b0a9a4a3a2a1b84a677ef8d2
jrmejiaa <jairo.mejia.2708@gmail.com>
2023-05-10 10:02:24 +0200

    cache_availability: Create var to differentiate Harvard vs Neumann arch

--- common/cache_availability.tcl
+++ common/cache_availability.tcl
@@ -13,3 +13,19 @@ set is_cache_available [list \
   "swerv_eh2_pe"    {false} \
   "cva5_pe"         {false} \
 ]
+
+set is_harvard_arch [list \
+  "vexriscv_pe"     {true} \
+  "orca_pe"         {true} \
+  "orca_mxp_pe"     {true} \
+  "piccolo32_pe"    {true} \
+  "piccolo64_pe"    {true} \
+  "flute32_pe"      {true} \
+  "flute64_pe"      {true} \
+  "picorv32_pe"     {false} \
+  "swerv_pe"        {true} \
+  "scr1_pe"         {true} \
+  "cva6_pe"         {true} \
+  "swerv_eh2_pe"    {true} \
+  "cva5_pe"         {true} \
+]
