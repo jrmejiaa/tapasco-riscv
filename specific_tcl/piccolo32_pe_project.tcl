8c933b2f9e936728f9356179ff28a27209cf81bc
jrmejiaa <jairo.mejia.2708@gmail.com>
2023-05-29 19:51:04 +0200

    piccolo32: Add compatibility with DDR Memory

--- specific_tcl/piccolo32_pe_project.tcl
+++ specific_tcl/piccolo32_pe_project.tcl
@@ -43,29 +43,56 @@ update_ip_catalog
 create_bd_intf_port -mode Slave -vlnv esa.informatik.tu-darmstadt.de:user:DMI_rtl:1.0 DMI
 connect_bd_intf_net [get_bd_intf_ports DMI] [get_bd_intf_pins piccolo_0/DMI]
 
+if {!$set_ddr_memory} {
+  if { $set_cache_sys && [dict get $is_cache_available $project_name] } {
+    proc create_specific_addr_segs {} {
+      variable lmem
+      # Create specific address segments
+      create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces cache_system_0/dmem] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
+      create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces cache_system_0/dmem] [get_bd_addr_segs rv_dmem_ctrl/S_AXI/Mem0] SEG_rv_dmem_ctrl_Mem0
+      create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces cache_system_0/imem] [get_bd_addr_segs rv_imem_ctrl/S_AXI/Mem0] SEG_rv_imem_ctrl_Mem0
+    }
 
-if { $set_cache_sys && [dict get $is_cache_available $project_name] } {
-  proc create_specific_addr_segs {} {
-    variable lmem
-    # Create specific address segments
-    create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces cache_system_0/dmem] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
-    create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces cache_system_0/dmem] [get_bd_addr_segs rv_dmem_ctrl/S_AXI/Mem0] SEG_rv_dmem_ctrl_Mem0
-    create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces cache_system_0/imem] [get_bd_addr_segs rv_imem_ctrl/S_AXI/Mem0] SEG_rv_imem_ctrl_Mem0
-  }
+    proc get_external_mem_addr_space {} {
+      return get_bd_addr_spaces cache_system_0/dmem
+    }
+  } else { # Not cache create address segments for only the core
+    proc create_specific_addr_segs {} {
+      variable lmem
+      # Create specific address segments
+      create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
+      create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs rv_dmem_ctrl/S_AXI/Mem0] SEG_rv_dmem_ctrl_Mem0
+      create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces piccolo_0/cpu_imem_master] [get_bd_addr_segs rv_imem_ctrl/S_AXI/Mem0] SEG_rv_imem_ctrl_Mem0
+    }
 
-  proc get_external_mem_addr_space {} {
-    return [get_bd_addr_spaces cache_system_0/dmem]
+    proc get_external_mem_addr_space {} {
+      return [get_bd_addr_spaces piccolo_0/cpu_dmem_master]
+    }
   }
-} else { # Not cache create address segments for only the core
-  proc create_specific_addr_segs {} {
-    variable lmem
-    # Create specific address segments
-    create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
-    create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs rv_dmem_ctrl/S_AXI/Mem0] SEG_rv_dmem_ctrl_Mem0
-    create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces piccolo_0/cpu_imem_master] [get_bd_addr_segs rv_imem_ctrl/S_AXI/Mem0] SEG_rv_imem_ctrl_Mem0
-  }
+} else {
+  if { $set_cache_sys && [dict get $is_cache_available $project_name] } {
+    proc create_specific_addr_segs {} {
+      variable lmem
+      # Create specific address segments
+      create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces cache_system_0/dmem] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
+      create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces cache_system_0/dmem] [get_bd_addr_segs M_AXI/Reg] M_AXI_DMem0
+      create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces cache_system_0/imem] [get_bd_addr_segs M_AXI/Reg] M_AXI_IMem0
+    }
 
-  proc get_external_mem_addr_space {} {
-    return [get_bd_addr_spaces piccolo_0/cpu_dmem_master]
+    proc get_external_mem_addr_space {} {
+      return [get_bd_addr_spaces cache_system_0/dmem]
+    }
+  } else { # Not cache create address segments for only the core
+    proc create_specific_addr_segs {} {
+      variable lmem
+      # Create specific address segments
+      create_bd_addr_seg -range 0x00010000 -offset 0x11000000 [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs RVController_0/saxi/reg0] SEG_RVController_0_reg0
+      create_bd_addr_seg -range $lmem -offset $lmem [get_bd_addr_spaces piccolo_0/cpu_dmem_master] [get_bd_addr_segs M_AXI/Reg] M_AXI_DMem0
+      create_bd_addr_seg -range $lmem -offset 0x00000000 [get_bd_addr_spaces piccolo_0/cpu_imem_master] [get_bd_addr_segs M_AXI/Reg] M_AXI_IMem0
+    }
+
+    proc get_external_mem_addr_space {} {
+      return [get_bd_addr_spaces piccolo_0/cpu_dmem_master]
+    }
   }
 }
